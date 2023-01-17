b5c83c52f46b69d638fdb148ad023fa25695ac9d
kx2471 <han29181@naver.com>
2023-01-17 21:16:26 +0900

    Loby, InputField

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,17 +14,47 @@ public class GameManager : MonoBehaviour
     public int score;
     public TMP_Text myScore;
     public TMP_Text myTime;
-    public float timeCount;
+    public float currentTime;
+
+    public TMP_Text question;
+
+    public string input;
+    public string randomWord;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTime = 3.5f;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        
+
         myScore.text = "나의 점수 : " + score;
+
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+            myTime.text = currentTime.ToString("F0");
+        }
+
+        
     }
+
+
+    public void ReadStringInput(string s)
+    {
+        input = s;
+        Debug.Log(input);
+        if (randomWord == input)
+        {
+            score++;
+        }
+
+        randomWord = word[Random.Range(0, 10)];
+        question.text = randomWord;
+    }
+
 }
